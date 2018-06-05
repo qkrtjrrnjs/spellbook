c2d0e8e216fa77186966f27881e753d8aecc17bb
Seok Kwun Park <park960@purdue.edu>
2018-06-05 17:25:24 -0400

    change graphics, input values, add new heading

--- index.js
+++ index.js
@@ -1,74 +1,54 @@
 const button = document.querySelector('button');
+let spellBook = [];
+let manaBook = [];
+let counter = 0;
 let spellTracker = 0;
-let spellingTracker = 0;
 
+//function that changes the heading texts 
 const changeText = function(){
-    const heading1 = document.querySelector('#spells');
-    const heading2 = document.querySelector('#spelling');
-    const spellBook = ['Wingardium Leviosa', 'Expelliarmus', 'Lumos', 'Alohomora', 'Avada Kedavra', 'Sectumsempra', 'Obliviate', 'Riddikulus', 'Accio', 'Expecto Patronum'];
-    const spelling = ['Techpoint', 'Al', 'Nichole', 'Fretless', 'Dana', 'Davey'];
+    const heading1 = document.querySelector('#SPELL');
+    const heading2 = document.querySelector('#MANA');
 
-    heading1.textContent = spellBook[spellTracker];
-    heading2.textContent = spelling[spellingTracker];
-    spellTracker++;
-    spellingTracker++;
-    if(spellTracker === 10)
+    if(spellTracker < spellBook.length){
+        heading1.textContent = spellBook[spellTracker];
+        heading2.textContent = manaBook[spellTracker++];
+    }
+    else{
         spellTracker = 0;
-    if(spellingTracker == 5)
-        spellingTracker = 0;
+        heading1.textContent = spellBook[spellTracker];
+        heading2.textContent = manaBook[spellTracker++];
+    }
+}
+
+//input "submit" type click handling
+const form = document.getElementById('formID');
+//form.usernameData.focus();
+form.onsubmit = function(event){
+    event.preventDefault();   
+    //const f = event.target; 
+    const spellValue = form.usernameData.value;//document.getElementById('u').value;
+    const manaValue = form.passwordData.value;//document.getElementById('p').value;
+    
+    if(spellValue === '' && manaValue === ''){
+        alert("ENTER SPELL AND MANA");
+    }
+    else if(spellValue === ''){
+        alert("ENTER SPELL");
+    }
+    else if(manaValue == ''){
+        alert("ENTER MANA");
+    }
+    else if(isNaN(manaValue)){
+        alert("ENTER A NUMBER FOR THE MANA VALUE");
+    }
+    else{
+        spellBook[counter] = spellValue;
+        manaBook[counter] = manaValue;
+        counter++;
+        form.reset();
+    }
 }
 
 button.addEventListener('click', changeText);
 
-
-const form = document.getElementById('formID');
-form.onsubmit = function(event){
-    event.preventDefault();    
-    const usernameValue = document.getElementById('u').value;
-    const passwordValue = document.getElementById('p').value;
-    const username = document.getElementById('user');
-    const password = document.getElementById('pass');
-
-    if(passwordValue != '' && usernameValue != ''){
-        username.innerHTML = 'username: ' + usernameValue;
-        password.innerHTML = 'password: ' + passwordValue;
-        form.reset();
-    }
-    
-    if(usernameValue === '' && passwordValue === ''){
-        alert("ENTER USERNAME AND PASSWORD");
-    }
-    else if(usernameValue === ''){
-        alert("ENTER USERNAME");
-    }
-    else if(passwordValue == ''){
-        alert("ENTER PASSWORD");
-    }
-}
-/*
-let userInput = document.getElementById("u");
-
-userInput.addEventListener("keyup", function(event) {
-  event.preventDefault();
-  if (event.keyCode === 13) {
-    const passwordValue = document.getElementById('p').value;
-    if(passwordValue === null){
-        alert("ENTER A PASSWORD");
-    }
-    document.getElementById("submitButton").click();
-  }
-});
-
-let passInput = document.getElementById("p");
-
-passInput.addEventListener("keyup", function(event) {
-  event.preventDefault();
-  if (event.keyCode === 13) {
-    const usernameValue = document.getElementById('u').value;
-    if(usernameValue === null){
-        alert("ENTER A USERNAME");
-    }
-    document.getElementById("submitButton").click();
-  }
-});*/
 
