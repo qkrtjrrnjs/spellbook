0fc9ab19f075f8a7b48a767e7622635f143448df
Seok Kwun Park <park960@purdue.edu>
2018-06-04 20:38:14 -0400

    create form, update p with text of text input, do not have to click button

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const button = document.querySelector('button');
 let spellTracker = 0;
 let spellingTracker = 0;
+
 const changeText = function(){
     const heading1 = document.querySelector('#spells');
     const heading2 = document.querySelector('#spelling');
     const spellBook = ['Wingardium Leviosa', 'Expelliarmus', 'Lumos', 'Alohomora', 'Avada Kedavra', 'Sectumsempra', 'Obliviate', 'Riddikulus', 'Accio', 'Expecto Patronum'];
     const spelling = ['Techpoint', 'Al', 'Nichole', 'Fretless', 'Dana', 'Davey'];
+
     heading1.textContent = spellBook[spellTracker];
     heading2.textContent = spelling[spellingTracker];
-
     spellTracker++;
     spellingTracker++;
     if(spellTracker === 10)
@@ -19,4 +20,54 @@ const changeText = function(){
 
 button.addEventListener('click', changeText);
 
+const form = document.getElementById('formID');
+form.onsubmit = function(event){
+    event.preventDefault();    
+    const usernameValue = document.getElementById('u').value;
+    const passwordValue = document.getElementById('p').value;
+    const username = document.getElementById('user');
+    const password = document.getElementById('pass');
+
+    if(passwordValue != '' && username != ''){
+        username.innerHTML = 'username: ' + usernameValue;
+        password.innerHTML = 'password: ' + passwordValue;
+        form.remove();
+    }
+    
+    if(usernameValue === '' && passwordValue === ''){
+        alert("ENTER USERNAME AND PASSWORD");
+    }
+    else if(usernameValue === ''){
+        alert("ENTER USERNAME");
+    }
+    else if(passwordValue == ''){
+        alert("ENTER PASSWORD");
+    }
+}
+/*
+let userInput = document.getElementById("u");
+
+userInput.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    const passwordValue = document.getElementById('p').value;
+    if(passwordValue === null){
+        alert("ENTER A PASSWORD");
+    }
+    document.getElementById("submitButton").click();
+  }
+});
+
+let passInput = document.getElementById("p");
+
+passInput.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    const usernameValue = document.getElementById('u').value;
+    if(usernameValue === null){
+        alert("ENTER A USERNAME");
+    }
+    document.getElementById("submitButton").click();
+  }
+});*/
 
