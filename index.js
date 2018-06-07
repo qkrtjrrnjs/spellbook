f264f14731a04c76e6382c72e3c8fe9c11fefd2c
Seok Kwun Park <park960@purdue.edu>
2018-06-07 11:32:06 -0400

    combine spell name and mana list, merge w/ Davey's code, change graphics

--- index.js
+++ index.js
@@ -1,70 +1,116 @@
-let spellBook = [];
-let manaBook = [];
-let counter = 0;
+class App{
+    constructor(){
+        this.spellBook = [];
+        this.template = document.querySelector('.spell.template')
 
-const list = function(data, listName){
-    var node = document.createElement('li');   
-    var btnNode = document.createElement('button');         
-    var textnode = document.createTextNode(data);       
-    var delTextNode = document.createTextNode('delete');
-    node.appendChild(textnode);                     
-    btnNode.appendChild(delTextNode);     
-    btnNode.onclick = function(){
-        this.parentNode.parentNode.removeChild(this.parentNode);
+        const form = document.querySelector('form')
+        form.addEventListener('submit', ev => {
+          this.handleSubmit(ev)
+        })
     }
-    document.getElementById(listName).appendChild(node).appendChild(btnNode);
-    node.style.color = '#363636';
-}
+
+    renderProperty(name, value) {
+        const el = document.createElement('span')
+        el.textContent = value
+        el.classList.add(name)
+        el.setAttribute('title', value)
+        return el
+      }
+    
+      renderItem(spell) {
+        const item = this.template.cloneNode(true)
+        item.classList.remove('template')
+    
+        // ['name', 'level', etc.]
+        const properties = Object.keys(spell)
+    
+        // Replace the appropriate values in each <span>
+        properties.forEach(property => {
+          const el = item.querySelector(`.${property}`)
+          el.textContent = spell[property]
+          el.setAttribute('title', spell[property])
+        })
+    
+        // delete button
+        item
+          .querySelector('button.delete')
+          .addEventListener(
+            'click',
+            this.removeSpell.bind(this, spell)
+          )
+    
+        // fav button
+    
+    
+        return item
+      }
+    
+      removeSpell(spell, ev) {
+        // Remove from the DOM
+        const button = ev.target
+        const item = button.closest('.spell')
+        item.parentNode.removeChild(item)
+    
+        // Remove from the array
+        const i = this.spellBook.indexOf(spell)
+        this.spellBook.splice(i, 1)
+      }
+
 
 //input "submit" type click handling
-const form = document.getElementById('formID');
-form.onsubmit = function(event){
-    event.preventDefault();   
-    //const f = event.target; 
-    const spellValue = form.spellData.value.trim();
-    const manaValue = form.manaData.value.trim();
-    
-    if(spellValue === '' && manaValue === ''){
-        alert("ENTER SPELL AND MANA");
-        form.spellData.focus();
-    }
-    else if(spellValue === ''){
-        alert("ENTER SPELL");
-        form.spellData.focus();
-    }
-    else if(manaValue == ''){
-        alert("ENTER MANA");
-        form.spellData.focus();
-    }
-    else{
-        const exists = false;
-        for(let i = 0; i < spellBook.length; i++){
-            if(spellBook[i] === spellValue){
-                form.spellData.focus();        
-                alert("SPELL ALREADY EXISTS IN THE SPELLBOOK!");
-                form.reset();   
-                exists = true;     
-            }
-        }
-        if(!exists){
-            list(spellValue, 'spellList');
-            list(manaValue, 'manaList');
+    handleSubmit(ev) {
+        ev.preventDefault();   
 
-            spellBook[counter] = spellValue;
-            manaBook[counter++] = manaValue;
-            spellBook.sort();
-            form.reset();
+        const form = ev.target
+
+        const spell = {
+            name: form.spellData.value.trim(),
+            manaNeeded: form.manaData.value.trim(),
+        }  
+    
+        if(spell.name === '' && spell.manaNeeded === ''){
+            alert("ENTER SPELL AND MANA");
             form.spellData.focus();
+        }
+        else if(spell.name === ''){
+            alert("ENTER SPELL");
+            form.spellData.focus();
+        }
+        else if(spell.manaNeeded == ''){
+            alert("ENTER MANA");
+            form.spellData.focus();
+        }
+        else{
+            let exists = false;
+            for(let i = 0; i < this.spellBook.length; i++){
+                if(this.spellBook[i].name === spell.name){
+                    form.spellData.focus();        
+                    alert("SPELL ALREADY EXISTS IN THE SPELLBOOK!");
+                    form.reset();   
+                    exists = true;     
+                }
+            }
+            if(!exists){
+                this.spellBook.push(spell);
+                const item = this.renderItem(spell);
+                const list = document.querySelector('#spellList');
+                list.appendChild(item);
+                form.reset();
+                form.spellData.focus();
+            }
         }
     }
 }
 
+new App();
+
 function makeVisible(){
-    document.querySelector('.gif').style.visibility='visible';
+    document.querySelector('.hp').style.visibility='visible';
 }
 
 function hide(){
-    document.querySelector('.gif').style.visibility='hidden';
+    document.querySelector('.hp').style.visibility='hidden';
+
 }
 
 
